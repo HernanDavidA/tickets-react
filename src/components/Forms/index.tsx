47eb558f3bd91c99bf8eb1e3eaf38dcfee8a0ab0
HernanDavidA <alvaremoreno111@gmail.com>
2025-03-18 16:16:41 -0500

    first commit

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -34,28 +34,28 @@ export default function FormComponent({ formData, errors, handleChange, handleSu
         </div>
   
         {/* Full Name */ }
-    <div className="mb-3 w-100">
-        <label htmlFor="name" className="form-label fw-bold text-light robotic-font-text">Full Name</label>
-        <input type="text" className="form-control bg-dark text-white border-0 rounded-3" id="name" name="name" placeholder="Enter your name" value={formData.name} onChange={handleChange} />
+    <div className="custom-input-container">
+        <label htmlFor="name" className="custom-label">Full Name</label>
+        <input type="text" className="custom-input" id="name" name="name" placeholder="Enter your name" value={formData.name} onChange={handleChange} />
         {errors.name && <p className="text-danger robotic-font-info"> <i className="info"> </i>{errors.name}</p>}
     </div>
 
     {/* Email */ }
-    <div className="mb-3 w-100">
-        <label htmlFor="email" className="form-label fw-bold text-light robotic-font-text">Email Address</label>
-        <input type="email" className="form-control bg-dark text-white border-0 rounded-3" id="email" name="email" placeholder="Enter your email" value={formData.email} onChange={handleChange} />
+    <div className="custom-input-container">
+        <label htmlFor="email" className="custom-label">Email Address</label>
+        <input type="email" className="custom-input" id="email" name="email" placeholder="Enter your email" value={formData.email} onChange={handleChange} />
         {errors.email && <p className="text-danger robotic-font-info">{errors.email}</p>}
     </div>
 
     {/* GitHub Username */ }
-    <div className="mb-3 w-100">
-        <label htmlFor="githubUsername" className="form-label fw-bold text-light robotic-font-text">GitHub Username</label>
-        <input type="text" className="form-control bg-dark text-white border-0 rounded-3" id="githubUsername" name="githubUsername" placeholder="@yourusername" value={formData.githubUsername} onChange={handleChange} />
+    <div className="custom-input-container">
+        <label htmlFor="githubUsername" className="custom-label">GitHub Username</label>
+        <input type="text" className="custom-input" id="githubUsername" name="githubUsername" placeholder="@yourusername" value={formData.githubUsername} onChange={handleChange} />
         {errors.githubUsername && <p className="text-danger robotic-font-info">{errors.githubUsername}</p>}
     </div>
 
     {/* Submit Button */ }
-    <button type="submit" className="btn btn-danger w-100 rounded-pill robotic-font-info">Generate My Ticket</button>
+    <button type="submit" className="btn btn-danger w-100 rounded-pill robotic-font-info mt-4">Generate My Ticket</button>
       </form >
   
     );
